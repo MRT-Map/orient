3dd075e9d310a1c1d321e6b47d8d8bd689bdc3ce
7d <61975820+iiiii7d@users.noreply.github.com>
2023-02-11 16:38:56 +0800

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+import com.soywiz.korge.gradle.*
+
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+	alias(libs.plugins.korge)
+}
+
+korge {
+	id = "io.github.iiiii7d.orient"
+// To enable all targets at once
+
+	//targetAll()
+
+// To enable targets based on properties/environment variables
+	//targetDefault()
+
+// To selectively enable targets
+
+	targetJvm()
+	//targetJs()
+	//targetDesktop()
+	//targetIos()
+	//targetAndroidIndirect() // targetAndroidDirect()
+}
