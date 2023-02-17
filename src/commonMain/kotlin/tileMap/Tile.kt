1eedea176d23dabf25fd7e86fd0a966937ee7746
7d <61975820+iiiii7d@users.noreply.github.com>
2023-02-18 01:47:18 +0800

    tile scaling in zooming

--- src/commonMain/kotlin/tileMap/Tile.kt
+++ src/commonMain/kotlin/tileMap/Tile.kt
@@ -8,14 +8,19 @@ import com.soywiz.korim.format.*
 import com.soywiz.korio.net.http.*
 import com.soywiz.korma.geom.*
 import com.soywiz.korma.interpolation.*
+import kotlin.math.pow
 
 suspend fun TileMap.tile(coord: TileCoord) =
-    Tile(coord).addTo(this).updatePosition(this).updateImage(this.settings)
+    Tile(coord, this.settings).addTo(this).updatePosition(this).updateImage()
 
 val imageCache = mutableMapOf<TileCoord, Bitmap>()
 
-class Tile(private val coord: TileCoord) : Container() {
-    suspend fun updateImage(settings: TileSettings): Tile {
+class Tile(private val coord: TileCoord, private val settings: TileSettings) : Container() {
+    private var image: Image? = null
+    private val imageSize
+        get() = 2.0.pow(15 - coord.z)
+
+    suspend fun updateImage(): Tile {
         val url = coord.url(settings)
         val client = HttpClient()
         try {
@@ -25,9 +30,10 @@ class Tile(private val coord: TileCoord) : Container() {
                     PNG.read(client.readBytes(url), filename = coord.toString())
                         .asumePremultiplied()
                 }
-            image(bitmap).size(128, 128).anchor(Anchor.TOP_LEFT).alpha(0).let {
-                it.tween(it::alpha[1], time = 0.5.seconds, easing = Easing.LINEAR)
-            }
+            image =
+                image(bitmap).size(imageSize, imageSize).anchor(Anchor.TOP_LEFT).alpha(0).also {
+                    it.tween(it::alpha[1], time = 0.5.seconds, easing = Easing.LINEAR)
+                }
         } catch (e: Http.HttpException) {
             if (e.statusCode != 404) throw e
         }
@@ -37,8 +43,8 @@ class Tile(private val coord: TileCoord) : Container() {
 
     fun updatePosition(map: TileMap): Tile {
         xy(
-            (coord.x * 128 + map.views.virtualWidth / 2).toDouble(),
-            (-coord.y * 128 - map.settings.maxZoomRange * 8.0 + map.views.virtualHeight / 2)
+            (coord.x * imageSize + map.views.virtualWidth / 2),
+            (-coord.y * imageSize - map.settings.maxZoomRange * 8.0 + map.views.virtualHeight / 2)
         )
         return this
     }
--- src/commonMain/kotlin/tileMap/TileMap.kt
+++ src/commonMain/kotlin/tileMap/TileMap.kt
@@ -1,16 +1,19 @@
 package tileMap
 
+import com.soywiz.klock.DateTime
 import com.soywiz.kmem.*
 import com.soywiz.korge.input.*
 import com.soywiz.korge.view.*
 import com.soywiz.korio.async.*
 import com.soywiz.korma.geom.*
+import kotlin.math.pow
+import kotlin.math.roundToInt
 import kotlinx.coroutines.*
 
 fun Container.tileMap(settings: TileSettings, views: Views) = TileMap(settings, views).addTo(this)
 
 class TileMap(var settings: TileSettings, val views: Views) : Container() {
-    var zoom = 8.0
+    var zoom = settings.maxTileZoom.toDouble()
     private var tiles = mutableMapOf<TileCoord, Deferred<Tile>>()
 
     val mouseWorldCoordinates
@@ -27,7 +30,12 @@ class TileMap(var settings: TileSettings, val views: Views) : Container() {
     }
 
     private suspend fun updateTilesShown() {
-        val centerTileCoord = TileCoord(8, worldOffset.x.toInt() / 32, worldOffset.y.toInt() / 32)
+        val centerTileCoord =
+            TileCoord(
+                zoom.roundToInt().toByte(),
+                worldOffset.x.toInt() / 32,
+                worldOffset.y.toInt() / 32
+            )
         val worldWidth = views.virtualWidth / 64
         val worldHeight = views.virtualHeight / 64
 
@@ -35,7 +43,7 @@ class TileMap(var settings: TileSettings, val views: Views) : Container() {
         for (x in
             (centerTileCoord.x - worldWidth / 2)..(centerTileCoord.x + worldWidth / 2)) for (y in
             (centerTileCoord.y - worldHeight / 2)..(centerTileCoord.y + worldHeight / 2)) {
-            val tileCoord = TileCoord(8, x, -y)
+            val tileCoord = TileCoord(zoom.roundToInt().toByte(), x, -y)
             newTiles[tileCoord] =
                 tiles[tileCoord] ?: async(Dispatchers.Unconfined) { tile(tileCoord) }
         }
@@ -47,6 +55,8 @@ class TileMap(var settings: TileSettings, val views: Views) : Container() {
         tiles = newTiles
     }
 
+    private var timeSinceLastScroll: Double? = null
+
     override fun onParentChanged() {
         val view: View = parent ?: this
 
@@ -56,6 +66,15 @@ class TileMap(var settings: TileSettings, val views: Views) : Container() {
             scrollAnywhere {
                 zoom += it.scrollDeltaYLines
                 zoom = zoom.clamp(0.0, 10.0)
+                scale(2.0.pow(zoom - (settings.maxTileZoom - 1.0)))
+                timeSinceLastScroll = DateTime.nowUnix()
+            }
+        }
+
+        addUpdater {
+            if (timeSinceLastScroll != null && DateTime.nowUnix() - timeSinceLastScroll!! > 500) {
+                launch(Dispatchers.Unconfined) { updateTilesShown() }
+                timeSinceLastScroll = null
             }
         }
     }
